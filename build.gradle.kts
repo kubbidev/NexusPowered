0105c85e4621502746fb2f0de45f89e633649b07
kubbi <nicoladelaroche@gmail.com>
2024-12-03 02:10:15 +0100

    add dmulloy2 protocollib repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://repo.dmulloy2.net/repository/public/")
 }
 
 dependencies {
