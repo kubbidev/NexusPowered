1c304bfacf4280b6ddb6a1c2fdc6938152e92ce2
kubbi <nicoladelaroche@gmail.com>
2025-02-14 14:12:09 +0100

    feat: add new tab completion

--- src/main/java/me/kubbidev/nexuspowered/command/tabcomplete/TabCompletions.java
+++ src/main/java/me/kubbidev/nexuspowered/command/tabcomplete/TabCompletions.java
@@ -1,14 +1,23 @@
 package me.kubbidev.nexuspowered.command.tabcomplete;
 
+import me.kubbidev.nexuspowered.util.Players;
+import org.bukkit.entity.HumanEntity;
+
 /**
  * Common completion suppliers used by plugins
  */
 public final class TabCompletions {
     private static final CompletionSupplier BOOLEAN = CompletionSupplier.startsWith("true", "false");
 
+    private static final CompletionSupplier PLAYERS = CompletionSupplier.startsWith(() -> Players.all().stream().map(HumanEntity::getName));
+
     // bit of a weird pattern, but meh it kinda works, reduces the boilerplate
 
     public static CompletionSupplier booleans() {
         return BOOLEAN;
+    }
+
+    public static CompletionSupplier players() {
+        return PLAYERS;
     }
 }
