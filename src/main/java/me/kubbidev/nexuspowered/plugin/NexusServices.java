527a6aa3db8943603a76fc86ac4a4eed1b9bc622
kubbi <nicoladelaroche@gmail.com>
2025-07-20 22:13:18 +0200

    Remove unused scoreboard provider setup logic

--- src/main/java/me/kubbidev/nexuspowered/plugin/NexusServices.java
+++ src/main/java/me/kubbidev/nexuspowered/plugin/NexusServices.java
@@ -1,19 +1,11 @@
 package me.kubbidev.nexuspowered.plugin;
 
-import me.kubbidev.nexuspowered.scoreboard.PacketScoreboardProvider;
-import me.kubbidev.nexuspowered.scoreboard.ScoreboardProvider;
-
 final class NexusServices {
 
     private NexusServices() {
     }
 
     static void setup(ExtendedJavaPlugin plugin) {
-        if (plugin.isPluginPresent("ProtocolLib")) {
-            PacketScoreboardProvider scoreboardProvider = new PacketScoreboardProvider(plugin);
-            plugin.provideService(ScoreboardProvider.class, scoreboardProvider);
-            plugin.provideService(PacketScoreboardProvider.class, scoreboardProvider);
-        }
     }
 
     private static boolean classExists(String clazz) {
